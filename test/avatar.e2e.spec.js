397d66de65b0bb0837784de6ee80fff0cc4778b9
Dm3583 <pankovdm@gmail.com>
2021-06-19 11:51:43 -0700

    e2e test complite

--- test/avatar.e2e.spec.js
+++ test/avatar.e2e.spec.js
@@ -16,7 +16,7 @@ const User = require('../model/user');
 const { testUser } = require('./data/data');
 
 describe('Test route user avatars', () => {
-    let user, token, re;
+    let user, token, re, file;
 
 
     beforeAll(async () => {
@@ -31,41 +31,35 @@ describe('Test route user avatars', () => {
     });
 
     afterAll(async () => {
+        unlink(path.join('tmp', file));
         const mongo = await db;
         await User.deleteOne({ email: testUser.email });
         await mongo.disconnect();
     });
 
     test('Upload user avatar success', async () => {
-        // const buf = await fs.readFile('./test/data/avatar.jpg');
-        // const buf = await fs.readFile.mockReturnValue(Promise.resolve(('./test/data/avatar.jpg')));
         fs.access.mockReturnValue(Promise.resolve(false));
-        console.log("START______________ ");
         const response = await supertest(app)
             .patch('/api/users/avatars')
             .set('Authorization', `Bearer ${token}`)
             .attach('avatar', './test/data/avatar.jpg')
-        // .attach('avatar', buf, 'avatar.jpg')
-        // console.log(response);
         expect(response.status).toEqual(200);
         expect(response.body).toBeDefined();
         expect(response.body.status).toEqual('success');
         expect(response.body.code).toEqual(200);
         expect(response.body.data.avatarURL).toBeDefined();
         expect(re.test(response.body.data.avatarURL)).toBeTruthy();
-        console.log("New path without folder ", response.body.data.avatarURL);
-        console.log(response.body);
+        file = response.body.data.avatarURL.toString().split('\\');
+        file = file[file.length - 1];
     });
 
     test('Upload user avatar fail token', async () => {
         const fakeToken = 'fgjhdgjfghkghlhjkkhdfsghfdgs.sdfgdfsgsfd.sfghfgh';
-        // const buf = await fs.readFile('./test/data/avatar.jpg');
         fs.unlink.mockReturnValue(Promise.resolve(false));
         const buf = await fs.readFile.mockReturnValue(Promise.resolve(('./test/data/avatar.jpg')));
         const response = await supertest(app)
             .patch('/api/users/avatars')
             .set('Authorization', `Bearer ${fakeToken}`)
-            // .attach('avatar', './test/data/avatar.jpg')
             .attach('avatar', buf, 'avatar.jpg')
         expect(response.status).toEqual(401)
     });
